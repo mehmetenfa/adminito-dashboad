f6ec1045b211373a4579a51581aa39973f565c49
qxan <mehmet.enfa@gmail.com>
2023-04-29 16:49:41 +0300

    common

--- src/common/Common.jsx
+++ src/common/Common.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from "react"
+import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined"
 
-const Common = () => {
+const Common = (props) => {
   return (
-    <div>Common</div>
+    <>
+      <div className='cardHeading'>
+        <h3>{props.title}</h3>
+        <MoreVertOutlinedIcon className='headingIcon' />
+      </div>
+    </>
   )
 }
 
