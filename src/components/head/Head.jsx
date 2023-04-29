b6b92e11f6806889d2ad4033e167b0535ff1dec1
qxan <mehmet.enfa@gmail.com>
2023-04-29 16:40:06 +0300

    headImports

--- src/components/head/Head.jsx
+++ src/components/head/Head.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React from "react"
+import SettingsIcon from "@mui/icons-material/Settings"
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined"
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined"
+import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined"
 
-const Head = () => {
+const Head = ({ dark, setMode }) => {
   return (
     <div>Head</div>
   )
